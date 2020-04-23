117809eedd0a698bf1def85739da26aa3a9a9587
ginowine <ginowine1@gmail.com>
2020-04-23 18:21:54 +0100

    service error fixed

--- src/main/java/com/example/DogRestApi/service/DogServiceImpl.java
+++ src/main/java/com/example/DogRestApi/service/DogServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.DogRestApi.service;
 import com.example.DogRestApi.entity.Dog;
 import com.example.DogRestApi.repository.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class DogServiceImpl implements DogService {
 
     @Autowired
