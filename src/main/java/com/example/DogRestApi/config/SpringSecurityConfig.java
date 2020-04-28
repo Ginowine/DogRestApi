bb8156f2daee2c1423bc48ad828f69201efca378
ginowine <ginowine1@gmail.com>
2020-04-28 08:19:29 +0100

    username and password method created

--- src/main/java/com/example/DogRestApi/config/SpringSecurityConfig.java
+++ src/main/java/com/example/DogRestApi/config/SpringSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.DogRestApi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -29,5 +32,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("admin")
                 .password(encoder().encode("password"))
                 .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
